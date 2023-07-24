b0ba4c174069600769bac44df5d9a45bc604f279
Xiaowei Ren <rxwgoodluck@hotmail.com>
2023-07-24 09:32:02 +0200

    home page

--- src/components/Acceuil/Acceuil.jsx
+++ src/components/Acceuil/Acceuil.jsx
@@ -6,22 +6,24 @@ import '../Acceuil/Acceuil.css';
 
 function Acceuil () {
     return(
-        <div className='home-container'>
-            <div className='home-container--left'>
-                <div className='home-container--left---text'>
-                    <p className='home-container--left---text1'>Hi, I am</p>
-                    <h2 className='home-container--left---title'>Xiaowei Ren</h2>
-                    <p className='home-container--left---text2'>I am a front-end developer and digital marketing expert. Nice to meet you !</p>
+        <div className='home'>
+            <div className='home-container container'>
+                 <div className='home-container--left'>
+                    <div className='home-container--left---text'>
+                        <p className='home-container--left---text1'>Hi, I am</p>
+                        <h2 className='home-container--left---title'>Xiaowei Ren</h2>
+                        <p className='home-container--left---text2'>I am a front-end developer and digital marketing expert. Nice to meet you !</p>
+                    </div>
+                    <button>DOWNLOAD CV</button>
+                    <div className='home-container--left---icon'>
+                        <img src={email} alt="email" />
+                        <img src={github} alt="github" />
+                        <img src={linkedin} alt="linkdin" />
+                    </div>
                 </div>
-                <button>DOWNLOAD CV</button>
-                <div className='home-container--left---icon'>
-                    <img src={email} alt="email" />
-                    <img src={github} alt="github" />
-                    <img src={linkedin} alt="linkdin" />
+                <div className='home-container-right'>
+                    <img src={illustration} alt="home page illustration" />
                 </div>
-            </div>
-            <div className='home-container-right'>
-                <img src={illustration} alt="home page illustration" />
             </div>
         </div>
     )  
