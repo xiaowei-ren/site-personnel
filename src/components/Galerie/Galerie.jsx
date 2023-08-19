1e025189c5bc59b6f6ef108648fa61c23363b882
Xiaowei Ren <rxwgoodluck@hotmail.com>
2023-08-19 18:07:46 +0200

    mis a jour des projets

--- src/components/Galerie/Galerie.jsx
+++ src/components/Galerie/Galerie.jsx
@@ -1,15 +1,22 @@
 import "../Galerie/Galerie.css"
+import link from "../../assets/link.png"
 
 function Galerie(props) {
+    {/*ver les liens de github */}
+    function openInNweTab(url) {
+        window.open(url, "_blank", "noreferrer")
+    };
+
     return(
         <div className='galerie-container'>
             <div className='img-container'>
                 <img src={props.img} alt="photo du projet" />
                 <div className='masque'></div>
             </div>
+            <img className='link' title="Voir le projet sur Github" src={link} alt="lien du projet vers github" onClick = {() => openInNweTab(props.url)}/>
             <div className='text-container'>
                 <h4>{props.title}</h4>
-                <p>{props.descrip}</p>
+                <p>{props.descrip}</p>               
             </div>
         </div>
     )
--- src/pages/Projet.jsx
+++ src/pages/Projet.jsx
@@ -2,15 +2,45 @@ import Galerie from "../components/Galerie/Galerie"
 import Title from "../components/Title/Title"
 import projet1 from "../assets/projet1.jpg"
 import "../style/Projet.css"
+import { useEffect, useState } from "react"
 
 function Projet() {
+
+    const [loading, setLoading] = useState(true)
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        if (data.length === 0 && loading) {
+            fetch('/data.json')
+                .then(response => response.json())
+                .then(data => {
+                    setData(data.projects)
+                    setLoading(false)
+                })
+                .catch(error => {
+                    console.error(error)
+                    setLoading(false)
+                });
+        }
+    }, [loading])
+    
+
     return(
         <div className='projet' id="projet">
             <div className='projet-container container'>
                 <Title title={"Projet"} descrip={"Projet informatique et projet du marketing"}/>
                 <div className='galerie-layout'>
-                    <Galerie img={projet1} title={"PROGRAMME"} descrip={"Développement d'un site de location de logement en React.js"}/>
-                    <Galerie img={projet1} title={"Programme"} descrip={"Développement d'un site de location de logement en React.js"}/>
+                    {data && data.length > 0 ? data.map((item, index) => {
+                        return (
+                            <Galerie
+                                img={item.img}
+                                title={item.title}
+                                descrip={item.descrip}
+                                url={item.link}
+                                key={"projects-" + index}
+                            />
+                        )
+                    }) : <></>}
                 </div>
             </div>
         </div>
